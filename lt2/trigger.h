0ba0af17b019a8081b6add42a35bcefd0629a1f0
sirmorris <charlie_robson@hotmail.com>
2019-09-03 17:39:17 +0100

    reinstate justreleased state

--- lt2/trigger.h
+++ lt2/trigger.h
@@ -25,28 +25,25 @@ public:
     }
 
     void update() {
-        int triggered = digitalRead(_pinNum) ? 1 : 0;
+        int pinState = digitalRead(_pinNum);
 
         _shiftreg <<= 1;
-        _shiftreg |= triggered;
+        _shiftreg |= pinState;
 
         switch(_state) {
             case NOTTRIGGERED:
-                if (triggered == 0) _state = JUSTTRIGGERED;
+                if (pinState == LOW) _state = JUSTTRIGGERED;
                 break;
             case JUSTTRIGGERED:
                 _state = HELD;
                 break;
             case HELD:
-                if ((_shiftreg & 0xff) == 0xff) _state = NOTTRIGGERED;
+                if (_shiftreg == 0xffff) _state = JUSTRELEASED;
+                break;
+            case JUSTRELEASED:
+                _state = NOTTRIGGERED;
                 break;
         }
-
-        // _state = NOTTRIGGERED;
-        // int b4 = _shiftreg & 0xff;
-        // if (b4 == 0xfe) _state = JUSTTRIGGERED;
-        // else if (b4 == 0x00) _state = HELD;
-        // else if (b4 == 0x01) _state = JUSTRELEASED;
 
         if (_state == HELD) {
             if (_counts != 0xffff) {
